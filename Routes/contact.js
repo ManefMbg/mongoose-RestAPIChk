09ef54d3d4514e335664b2cdab6c3113a043d651
ManefMbg <manefbenghorbel@gmail.com>
2024-03-10 22:58:30 +0100

    first commit

---
+++ Routes/contact.js
@@ -0,0 +1,63 @@
+
+const express = require ('express');
+const Contact = require('../models/Contact');
+
+
+const router = express.Router();
+ 
+router.get('/test',(req,res)=>{
+    res.send("Hello world, I'm here!!!");
+});
+
+router.post("/add", async (req,res)=>{
+    try {
+        const {name, email, phone} = req.body;
+        const newContact = new Contact({name, email, phone});
+        await newContact.save();
+        res.status(200).send({ message:"Contact added successuflly", newContact});
+    } catch (error) {
+        res.status(400).send({message:"Can't added contact!!",error})
+    }
+});
+
+router.get('/all', async (req,res)=>{
+    try {
+        const listContact= await Contact.find();
+        res.status(200)
+        .send({msg: "Contacts received", listContact});            
+    } catch (error) {
+        res.status(400).send({msg:"Can't get, sorry!"});        
+    }
+});
+
+router.get('/:id', async (req,res)=>{
+    try {
+       const contactToGet = await Contact.findOne({_id: req.params.id});
+       res.status(200).send({msg:"Contact received", contactToGet});
+    } catch (error) {
+        res.status(400).send({msg:"id doesn't exist", error});    
+    }
+});
+
+router.delete('/:_id', async (req,res)=>{
+    try {
+       const { _id } = req.params;
+       await Contact.findOneAndDelete({ _id });
+       res.status(200).send({msg:"Contact deleted"});
+    } catch (error) {
+        res.status(400).send({msg:"Can't delete contact with this id !", error});    
+    }
+});
+
+router.put('/:_id', async (req,res)=>{
+    try {
+       const { _id } = req.params;
+       const result= await Contact.updateOne({ _id },{$set:{...req.body}});
+       res.status(200).send({msg:"Contact updated"});
+    } catch (error) {
+        res.status(400).send({msg:"Can't update contact with this id !", error});    
+    }
+});
+
+
+module.exports = router;
---
+++ config/connectDB.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.DB_URI);
+        console.log("Database connected...");
+    }  
+    
+    catch (error){
+        console.log('Can not connected!!!', error)
+        
+    }       
+    };
+
+module.exports = connectDB;
