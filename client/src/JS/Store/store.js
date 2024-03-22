d8854a9702356711fb45676a9f72a74cef68ccc7
ManefMbg <manefbenghorbel@gmail.com>
2024-03-22 20:31:37 +0100

    4th commit

--- client/src/JS/Store/store.js
+++ client/src/JS/Store/store.js
@@ -1,7 +1,7 @@
 
 import { createStore, applyMiddleware,compose } from "redux";
 import rootReducer from "../Reducers";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 
 
